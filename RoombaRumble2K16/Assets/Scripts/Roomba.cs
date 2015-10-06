ca18c83cd26381231bf660be4d09d77ae65ce87b
Steven Dill <sdd4163@rit.edu>
2015-10-06 11:27:55 -0400

    Remade RoombaPrefab and Game Manager. Added edge colliders to plane

--- RoombaRumble2K16/Assets/Scripts/Roomba.cs
+++ RoombaRumble2K16/Assets/Scripts/Roomba.cs
@@ -15,6 +15,8 @@ public class Roomba : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (health <= 0) {
+			GetComponent<CharacterController>().enabled = false;
+		}
 	}
 }
