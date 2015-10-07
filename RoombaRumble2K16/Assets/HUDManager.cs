aee7d15969f19b325db33c719cde4f5c064bf6e5
Steven Dill <sdd4163@rit.edu>
2015-10-07 13:53:33 -0400

    Started working on HUD

---
+++ RoombaRumble2K16/Assets/HUDManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = 99.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		Canvas canv = GameObject.Find ("Canvas").GetComponent<Canvas>();
+		int tempTimer = (int)timer;
+		canv.GetComponentInChildren<Text> ().text = tempTimer.ToString();
+	}
+}
