d9dc7813a435d2753632d0db84a35e7730ba770e
unknown <pjl7021@GDD-61.main.ad.rit.edu>
2015-10-12 12:17:51 -0400

    Started weapon functionality: - weapon class - buzzsaw working - collision ready to use

---
+++ RoombaRumble2K16/Assets/Scripts/Buzzsaw.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buzzsaw : Weapon {
+
+    public GameObject buzzsaw1;
+    public GameObject buzzsaw2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public override void Fire ()
+    {
+        buzzsaw1.transform.Rotate(Vector3.forward * 720 * Time.deltaTime);
+        buzzsaw2.transform.Rotate(Vector3.forward * -720 * Time.deltaTime);
+    }
+}
--- RoombaRumble2K16/Assets/Scripts/RoombaController.cs
+++ RoombaRumble2K16/Assets/Scripts/RoombaController.cs
@@ -9,6 +9,9 @@ public class RoombaController : MonoBehaviour {
     private float friction;
     private int playerID;
 
+    GameObject meleeWeapon;
+    public GameObject meleePrefab;
+
 	// Use this for initialization
 	void Start () {
         speed = 0f;
@@ -16,6 +19,9 @@ public class RoombaController : MonoBehaviour {
         acceleration = .5f;
         friction = .9f;
         playerID = this.GetComponent<Roomba>().PlayerID;
+
+        meleeWeapon = (GameObject)Instantiate(meleePrefab, transform.position, transform.rotation);
+        meleeWeapon.transform.parent = this.transform;
 	}
 	
 	// Update is called once per frame
@@ -35,6 +41,10 @@ public class RoombaController : MonoBehaviour {
         if ((playerID == 1 && Input.GetKey(KeyCode.D)) || (playerID == 2 && Input.GetKey(KeyCode.RightArrow)))
         {
             transform.Rotate(Vector3.forward * -180 * Time.deltaTime);
+        }
+        if((playerID == 1 && Input.GetKey(KeyCode.B)) || (playerID == 2 && Input.GetKey(KeyCode.Keypad1)))
+        {
+            meleeWeapon.GetComponent<Weapon>().Fire();
         }
 
         if(speed > maxSpeed)
