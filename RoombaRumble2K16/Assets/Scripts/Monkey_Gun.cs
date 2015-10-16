2437fe988159d3ef128566a334433a00b4ed5ebd
unknown <ixi7500@GDD-49.main.ad.rit.edu>
2015-10-16 14:29:41 -0400

    Organized and removed unnecessary assets, created UI and Effect Folders, added Monkey gun and poo (Collision Broken!!!!!)

---
+++ RoombaRumble2K16/Assets/Scripts/Monkey_Gun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monkey_Gun : Weapon {
+    public GameObject monkeyGun;
+    public GameObject projectile;
+
+    private bool canFire;
+    // Use this for initialization
+    void Start () {
+        canFire = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    public override void Fire()
+    {
+        if (canFire)
+        {
+            StartCoroutine(Launch());
+        }
+    }
+    // method to fire poop
+    IEnumerator Launch()
+    {
+        canFire = false;
+        Instantiate(projectile, monkeyGun.transform.position, monkeyGun.transform.rotation);
+        yield return new WaitForSeconds(.2f);
+        canFire = true;
+    }
+}
